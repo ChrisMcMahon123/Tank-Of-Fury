df6b0ed092cfa405bf37d46d427b38119a609995
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2017-11-21 11:44:30 -0500

    first
    first

---
+++ Tanks of Fury/Assets/Scripts/FireCannon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCannon : MonoBehaviour {
+	public ParticleSystem cannonFire;//animation for cannon fire
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("space")) {
+			cannonFire.Play ();
+		} else {
+			cannonFire.Stop ();
+		}
+	}
+}
---
+++ Tanks of Fury/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+	int speed;
+	// Use this for initialization
+	void Start () {
+		speed = 25;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//moving the player based on user input
+		if (Input.GetKey("up"))
+		{
+			//move object in the forward direction thats its facing
+			Vector3 movement = transform.forward;
+			movement.y = transform.position.y;//preserve the y axis to prevent the camera from moving down
+			transform.position += movement * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey("down"))
+		{
+			//move object in the backward direction thats its facing
+			Vector3 movement = transform.forward;
+			movement.y = transform.position.y;//preserve the y axis to prevent the camera from moving down
+			transform.position -= movement * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey("left"))
+		{
+			//rotate camera left along y axis
+			transform.RotateAround(transform.position, Vector3.down, 2 * speed * Time.deltaTime);//rotate around self
+		}
+
+		if (Input.GetKey("right"))
+		{
+			//move camera right along y axis
+			transform.RotateAround(transform.position, Vector3.up, 2 * speed * Time.deltaTime);//rotate around self
+		}
+	}
+}
