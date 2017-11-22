cbd3c4aba0b88ce48787750bf8ad94638f9e2a07
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2017-11-22 12:02:17 -0500

    AOE with Triggers
    Objects die when trigger hits them now

--- Tanks of Fury/Assets/Scripts/GrowAOE.cs
+++ Tanks of Fury/Assets/Scripts/GrowAOE.cs
@@ -4,32 +4,22 @@ using UnityEngine;
 
 public class GrowAOE : MonoBehaviour {
 	float aoeSpeed;
-	int min;
-	int max;
 
 	void Start () {
-		aoeSpeed = 5.0f;
-		min = 1;
-		max = 3;
+		aoeSpeed = 1;
 	}
 
 	void Update () {
-		//if(transform.localScale.x >= 50)
-	//	{
-			//grow the AOE until it hits a specific size and then destroy it
-			transform.localScale = new Vector3(min + Mathf.PingPong(Time.time, max), min + Mathf.PingPong(Time.time, max), min + Mathf.PingPong(Time.time, max));	//	}
-	//	else {
-			//Destroy(gameObject);
-	//	}
-	}
+		aoeSpeed+= 0.1f;
 
-	void OnTriggerEnter(Collider collider)
-	{
-		//if (collider.tag == "AOEDamage") {
-		//destroy the object anytime an object with the AOEDamage tag comes into contact with it
-		//emulating the ways of destructable environments
-		//Destroy (gameObject);
-		Debug.Log("Environment Piece Destroyed by AOE");
-		//}
+		if(transform.localScale.x < 10)
+	 	{
+			//grow the AOE until it hits a specific size and then destroy it
+			transform.localScale = new Vector3(1,1,1) * aoeSpeed;
+		}	
+		else 
+		{
+			Destroy(gameObject);
+		}
 	}
 }
