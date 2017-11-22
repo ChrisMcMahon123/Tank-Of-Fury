5a87ad4705939bba2affa4e4819e6887490e55f1
JoseAlba <josealba1996@hotmail.com>
2017-11-22 14:10:07 -0500

    Procedural Generation

---
+++ Assets/Scripts/RotateCannon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCannon : MonoBehaviour {
+	void Update () {
+		if (Input.GetKey("z"))
+		{
+			if (transform.localEulerAngles.y > 140) {
+				//rotate the cannon up
+				transform.Rotate (Vector3.left * 2 * GlobalVariables.moveSpeed * Time.deltaTime);
+			} 
+		}
+
+		if (Input.GetKey ("c")) {
+			if (transform.localEulerAngles.y < 180) {
+				//rotate the cannon down
+				transform.Rotate (Vector3.right * 2 * GlobalVariables.moveSpeed * Time.deltaTime);
+			}
+		}
+	}
+}
