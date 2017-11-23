29874fd6354ee3ed790ce289f969c30bc3140509
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2017-11-22 21:57:13 -0500

    Health Bar
    Health Bar and script fully working with AOEDamage Tag

--- Tanks of Fury/Assets/Scripts/GlobalVariables.cs
+++ Tanks of Fury/Assets/Scripts/GlobalVariables.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class GlobalVariables : MonoBehaviour {
 	//global variables that any script can access
 	public static int moveSpeed = 50;
-	public static int jumpSpeed = 500;
+	public static int jumpSpeed = 1000;
 	public static int cannonMultiplier = 1000;
 	public static float coolDownPeriod = 1.0f;
 	public static float aoeSpeed = 1;
+	public static int playerOneCurrentHealth;
+	public static int playerOneMaxHealth;
 }
