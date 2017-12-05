ab26549cf2ef1cd306b3bf7ebf564d85d3793416
JoseAlba <josealba1996@hotmail.com>
2017-12-05 12:04:02 -0500

    Final Game

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+
+	public void ChangeToScene(string sceneToChangeTo){
+		SceneManager.LoadScene(sceneToChangeTo);
+		Time.timeScale = 1;
+		if (sceneToChangeTo == "PVP") {
+			GlobalVariables.mode = "PVP";
+		} else if (sceneToChangeTo == "PVE") {
+			GlobalVariables.mode = "PVM";
+		}
+	}
+}
